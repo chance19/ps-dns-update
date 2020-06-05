489534327f69a5645019208488d17e05cfe5d123
Chance <chance19@gmail.com>
2020-06-05 14:30:04 +0100

    update comments

---
+++ DNSUpdate.ps1
@@ -0,0 +1,20 @@
+﻿# Powershell script to update the DNS entries of a Windows based machine.
+
+# Get the current dns servers applied to all network interfaces
+$currentDNS = Get-DnsClientServerAddress
+
+# Loop through each interface and determine which interfaces need updating
+foreach($obj in $currentDNS)
+{
+    # If the interface currently has an entry that is like 192.168.102.16 then run the Set-DnsClientServerAddress command, else, don't make a change
+    if ($obj.Address -like "192.168.10.10"){
+        Write-host $obj.InterfaceAlias
+        Write-host "Interface ID : $($obj.name) : Has DNS :  $($obj.address)" 
+        Set-DnsClientServerAddress -InterfaceIndex $obj.name -ServerAddresses ("192.168.10.10","192.168.10.12","10.1.0.10","10.2.0.10")
+    } else {
+        Write-host "Interface ID : $($obj.name) : No DNS entries, no change" 
+    }
+}
+
+# IP Config to check that the change has taken affect.
+ipconfig /all
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ps-dns-update
  
+This powershell script can be applied to windows based machines to update dns server entries on the local network adapter.
+
+The script checks to see if the adapter currently has a certain entry and if it does it will run a set command that can either change or update the dns server entries.
